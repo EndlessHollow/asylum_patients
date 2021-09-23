cac916ea1b9fcdae0cfe850d0fc2f7be91d04231
Marek Klier <marek.klier.personal@gmail.com>
2021-09-23 20:55:13 +0200

    feat: add initial project setup (#1)
    Co-authored-by: EndlessHollow <marekklier.personal@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { GlobalStyle } from "./utils/global-style";
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <div> App </div>
+    </>
+  );
+};
+
+App.displayName = "App";
+
+export default App;
