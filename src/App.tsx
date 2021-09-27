58496975d45bc86298b1e3f72623dfa989359ef0
EndlessHollow <marekklier.personal@gmail.com>
2021-09-27 21:06:04 +0200

    feat: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import * as patientsData from "./data/patients.json";
+import { Dashboard } from "./modules/dashboard/dashboard";
 import { GlobalStyle } from "./utils/global-style";
+import theme from "./utils/theme";
+
 const App = () => {
+  const { default: data } = JSON.parse(JSON.stringify(patientsData));
+
   return (
     <>
       <GlobalStyle />
-      <div> App </div>
+      <ThemeProvider theme={theme}>
+        <Dashboard data={data} />
+      </ThemeProvider>
     </>
   );
 };
